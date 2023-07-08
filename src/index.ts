0f48024b4d6113c42290150ffd78641455e8a6f4
Theodros Yimer <12742021+theodrosyimer@users.noreply.github.com>
2023-07-08 17:57:52 +0200

    feat: add multiply function

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,10 @@ export function add(a: number, b: number) {
   return a + b
 }
 
-export function divide(a: number,b: number) {
+export function divide(a: number, b: number) {
   return a / b
+}
+
+export function mutiply(a: number, b: number) {
+  return a * b
 }
